1b1580a5df7aa52b10728edbe2a08e6ff7b4f9d7
LeonardoVileela <leovilela.empresa@gmail.com>
2021-01-11 14:25:04 -0400

    site web mv contruçoes adicioando

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res, next) {
 
   const sgMail = require('@sendgrid/mail');
-  key = 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
+  key = NlEatzppS12sCFipneEPdQ.CYkqi3A6CnCrsBVAaGNW75LjTJ_e1Ek1yeX8Dg1alWM
   sgMail.setApiKey(process.env.key);
   const msg = {
     to: 'leoalmeida.empresa@gmail.com',
