6e808f2072145a919de517683cd5b5f08f191ad9
Yuumi <leovilela.empresa@gmail.com>
2021-01-21 23:42:00 -0300

    site puro

--- app.js
+++ app.js
@@ -3,47 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session')
-var MySQLStore = require('express-mysql-session')(session);
 var indexRouter = require('./routes/index');
 var app = express();
-var db = require('./model/db')
-
-setInterval(function () {
-  db.query('SELECT * FROM users');
-}, 5000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-/*
-var options = {
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'session'
-};
-*/
-
-var options = {
-  host: 'mysql742.umbler.com',
-  port: 3306,
-  user: 'db-root',
-  password: 'leo91167213',
-  database: 'session'
-};
-
-var sessionStore = new MySQLStore(options);
-
-app.use(session({
-  key: 'yuumi',
-  secret: 'yuumi',
-  store: sessionStore,
-  resave: true,
-  saveUninitialized: true
-}));
 
 function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
