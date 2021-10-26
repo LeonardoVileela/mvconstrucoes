107ceaf7dff85d26acf18b15fcf75adca6836860
LeonardoVileela <leovilela.empresa@gmail.com>
2021-10-26 14:29:21 -0400

    chatbot with margin

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ app.set('view engine', 'ejs');
 
 function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+  /*if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
     return res.redirect('https://' + req.get('host') + req.url);
-  }
+  }*/
+  return res.redirect('https://mvconstrucoesms.com.br');
   next();
 }
 
