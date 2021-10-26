5f64fe10a01e3e946ad11d80b8a0f6bd6b516414
LeonardoVileela <leovilela.empresa@gmail.com>
2021-10-26 11:58:24 -0400

    chatbot

---
+++ public/main.js
@@ -0,0 +1,14 @@
+const popup = document.querySelector('.chat-popup');
+const chatBtn = document.querySelector('.chat-btn');
+const submitBtn = document.querySelector('.submit');
+const chatArea = document.querySelector('.chat-area');
+const inputElm = document.querySelector('input');
+const emojiBtn = document.querySelector('#emoji-btn');
+const picker = new EmojiButton();
+
+       
+//   chat button toggler 
+
+chatBtn.addEventListener('click', ()=>{
+    popup.classList.toggle('show');
+})
