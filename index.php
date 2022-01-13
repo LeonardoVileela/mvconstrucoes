79015c450dcc32d6d06048f49647650e2069f0e0
LeonardoVileela <leovilela.empresa@gmail.com>
2022-01-13 09:56:40 -0400

    https

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
-<?php
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-  $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-  header('HTTP/1.1 301 Moved Permanently');
-  header('Location: ' . $location);
-  exit;
-}
-?>
+<?php ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
